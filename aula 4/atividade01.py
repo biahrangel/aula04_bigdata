659b381a29666d1d16a35dd3359bdaaf3421a761
biahrangel <beatrizrangel.m@gmail.com>
2026-08-12 19:39:43 -0300

    avaliar maior idade

---
+++ aula 4/atividade01.py
@@ -0,0 +1,9 @@
+# verifica a maior idade
+
+idade = int (input('Digite a idade: '))
+
+if idade >= 18:
+    print('maior de idade!') 
+
+else:
+    print('menor de idade!')
--- aula 4/exemplo01.py
+++ aula 4/exemplo01.py
@@ -1,14 +1,20 @@
 # ex01
 # uma instituicao de ensino precisa calcular a media bimestral dos alunos.
 # a avaliacao é composta por teste e prova. 
-# crie um algoritmo que solicie as duas notas ao usuario, calcule a media e imprima o resultado 
+# crie um algoritmo que solicite as duas notas ao usuario, calcule a media e imprima o resultado 
+
+#entrada
 
 teste = float (input('insira a nota do teste: '))
 prova = float (input('insira a nota da prova: '))
 
+#desenvolvimento 
 
 soma_teste_prova = teste + prova
 media_bimestre = soma_teste_prova  / 2 
 
-print(f'nota final do bimestre: {media_bimestre}')
+
+#saida
+
+print(f'nota final do bimestre: {media_bimestre:.2f} ')
 
